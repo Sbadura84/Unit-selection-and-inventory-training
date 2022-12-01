75c52ead598888caeb6919014026e6e701db837c
MrocznyKnajt <MrocznyKnajt@gmail.com>
2022-12-01 01:35:25 +0100

    Random fiddling with Inventory

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ItemPickup : Interactable
+{
+    public Item item;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        Pickup();
+    }
+
+    void Pickup()
+    {
+        bool wasPickedUp = Inventory.Instance.Add(item);
+     if (wasPickedUp)
+        {
+
+      //  Destroy(GameObject);
+        }
+    }
+}
